d0c50f929adbd87b83c8d2fa5619ddedb884ba78
soydiego <diegofranchina@gmail.com>
2022-02-01 12:38:08 +0100

    Added StyleSheet

--- 01-MyFirstApp/src/screens/CounterScreen.tsx
+++ 01-MyFirstApp/src/screens/CounterScreen.tsx
@@ -1,16 +1,32 @@
 /* eslint-disable react-native/no-inline-styles */
-import {View, Text, Button} from 'react-native';
+import {View, Text, TouchableOpacity, StyleSheet} from 'react-native';
 import React, {useState} from 'react';
+import {tsLiteralType} from '@babel/types';
 
 const CounterScreen = () => {
   const [counter, setCounter] = useState(10);
 
   return (
-    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-      <Text style={{fontSize: 50}}>Counter: {counter} </Text>
-      <Button title="+1" onPress={() => setCounter(counter + 1)} />
+    <View style={styles.container}>
+      <Text style={styles.title}>Counter: {counter} </Text>
+      <TouchableOpacity onPress={() => setCounter(counter + 1)}>
+        <View>
+          <Text>+1</Text>
+        </View>
+      </TouchableOpacity>
     </View>
   );
 };
 
 export default CounterScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  title: {
+    fontSize: 50,
+  },
+});
