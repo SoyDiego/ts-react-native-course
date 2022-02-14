2de92033a8031130b121062f102f0c6dbfa76d58
soydiego <diegofranchina@gmail.com>
2022-02-14 18:11:02 +0100

    Added screenOptions, drawerType and options in Drawer.Screen

--- 03-navigationApp/src/navigator/LateralMenuBasic.tsx
+++ 03-navigationApp/src/navigator/LateralMenuBasic.tsx
@@ -2,14 +2,29 @@ import React from 'react';
 import {createDrawerNavigator} from '@react-navigation/drawer';
 import StackNavigator from './StackNavigator';
 import SettingsScreen from '../screens/SettingsScreen';
+import {useWindowDimensions} from 'react-native';
 
 const LateralMenuBasic = () => {
   const Drawer = createDrawerNavigator();
 
+  const {width} = useWindowDimensions();
+
   return (
-    <Drawer.Navigator initialRouteName="StackNavigator">
-      <Drawer.Screen name="StackNavigator" component={StackNavigator} />
-      <Drawer.Screen name="SettingsScreen" component={SettingsScreen} />
+    <Drawer.Navigator
+      initialRouteName="StackNavigator"
+      screenOptions={{
+        drawerType: width >= 768 ? 'permanent' : 'front',
+      }}>
+      <Drawer.Screen
+        name="StackNavigator"
+        options={{title: 'Home'}}
+        component={StackNavigator}
+      />
+      <Drawer.Screen
+        name="SettingsScreen"
+        options={{title: 'Settings'}}
+        component={SettingsScreen}
+      />
     </Drawer.Navigator>
   );
 };
