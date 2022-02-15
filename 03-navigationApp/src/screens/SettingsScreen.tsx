4894df57811527e9c5616f0efc09f0fd1a629a46
soydiego <diegofranchina@gmail.com>
2022-02-15 12:38:16 +0100

    useSafeAreInserts hook

--- 03-navigationApp/src/screens/SettingsScreen.tsx
+++ 03-navigationApp/src/screens/SettingsScreen.tsx
@@ -1,10 +1,14 @@
 import {View, Text} from 'react-native';
 import React from 'react';
+import {useSafeAreaInsets} from 'react-native-safe-area-context';
+import {styles} from '../themes/appTheme';
 
 const SettingsScreen = () => {
+  const insets = useSafeAreaInsets();
+
   return (
-    <View>
-      <Text>SettingsScreen</Text>
+    <View style={{...styles.globalMargin, marginTop: insets.top}}>
+      <Text style={styles.title}>SettingsScreen</Text>
     </View>
   );
 };
