0e1285f6c3fa9e70a151ee6cc5e32daa52fdc2bc
soydiego <diegofranchina@gmail.com>
2022-02-17 16:15:35 +0100

    Installed icons in Android

--- 03-navigationApp/src/screens/Tab1Screen.tsx
+++ 03-navigationApp/src/screens/Tab1Screen.tsx
@@ -1,10 +1,15 @@
 import {View, Text} from 'react-native';
 import React from 'react';
+import {styles} from '../themes/appTheme';
+import Icon from 'react-native-vector-icons/Ionicons';
 
 const Tab1Screen = () => {
   return (
-    <View>
-      <Text>Tab1Screen</Text>
+    <View style={styles.globalMargin}>
+      <Text style={styles.title}>Íconos</Text>
+      <Text>
+        <Icon name="airplane-outline" size={30} color="#900" />;
+      </Text>
     </View>
   );
 };
