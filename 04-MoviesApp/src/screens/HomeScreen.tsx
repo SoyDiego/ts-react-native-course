ea48fafc2dc1c026a4fbc3e2856ff908d42d532c
soydiego <diegofranchina@gmail.com>
2022-02-24 15:36:50 +0100

    Installed Carousel and showed the movies

--- 04-MoviesApp/src/screens/HomeScreen.tsx
+++ 04-MoviesApp/src/screens/HomeScreen.tsx
@@ -1,10 +1,12 @@
-import {View, Text, ActivityIndicator} from 'react-native';
+import {View, Text, ActivityIndicator, Dimensions} from 'react-native';
 import React from 'react';
+import Carousel from 'react-native-snap-carousel';
 import useMovies from '../hooks/useMovies';
 import MoviePoster from '../components/MoviePoster';
 import {useSafeAreaInsets} from 'react-native-safe-area-context';
 
 const HomeScreen = () => {
+  const {width: windowWidth} = Dimensions.get('window');
   const {moviesInCinema, isLoading} = useMovies();
 
   const {top} = useSafeAreaInsets();
@@ -19,7 +21,17 @@ const HomeScreen = () => {
 
   return (
     <View style={{marginTop: top + 20}}>
-      <MoviePoster movie={moviesInCinema[0]} />
+      {/* <MoviePoster movie={moviesInCinema[0]} /> */}
+      <View style={{height: 440}}>
+        <Carousel
+          autoplay={true}
+          autoplayInterval={3000}
+          data={moviesInCinema}
+          renderItem={({item}) => <MoviePoster movie={item} />}
+          sliderWidth={windowWidth}
+          itemWidth={300}
+        />
+      </View>
     </View>
   );
 };
